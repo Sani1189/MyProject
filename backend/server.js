56e11e3906b84895f0fcad4ce7be2cf9825f609e
Saniul Islam Sani <76781606+Sani1189@users.noreply.github.com>
2024-11-07 04:08:21 +0600

    done

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.get('/api/data', async (req, res) => {
 });
 
 // Start the server
-const PORT =  5000;
+const PORT =  process.env.PORT || 5000 ;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
