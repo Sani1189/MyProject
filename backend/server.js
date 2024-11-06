316d09749564215defb749bf2e646a0c5f056dd4
Saniul Islam Sani <76781606+Sani1189@users.noreply.github.com>
2024-11-07 03:59:48 +0600

    done

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect("mongodb+srv://sani1189:sani1189@dummy.hg1ak.mongodb.net/testData?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
@@ -47,7 +47,7 @@ app.get('/api/data', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
